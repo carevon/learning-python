dd82d11977264429b24f84840fcb4a3b40495a75
Sir Felipe <felipe.dioliveira@outlook.com>
2019-07-03 21:21:37 -0300

    Python basics

---
+++ basics/print.py
@@ -0,0 +1,14 @@
+# PYTHON BASICS - PRINT FUNCTION
+
+subst = "Python"
+verb = "is"
+adjective = "fantastic"
+print(subst, verb, adjective, sep="_", end="!\n")
+
+
+# IMPRIMINDO UMA DATA
+dia = "07"
+mes = "07"
+ano = "1993"
+
+print(dia, mes, ano, sep="/")
