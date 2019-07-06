1a47bf3efbde9cf4ba450a2f1de31353efbb89b1
Sir Felipe <felipe.dioliveira@outlook.com>
2019-07-06 20:49:14 -0300

    XPTO

--- basics/adivinhacao.py
+++ basics/adivinhacao.py
@@ -3,26 +3,24 @@ print("Seja bem-vindo ao jogo de adivinhação")
 print("****************************************")
 
 numero_secreto = 42
-rodada = 1
-total_tentativas = 3
+total_de_tentativas = 3
 
-while (rodada <= total_tentativas):
-    print("Tentativa {} de {}".format(rodada, total_tentativas))
-    chute_str = input("Digite o número:")
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
-    acertou = chute == numero_secreto
+    acertou = numero_secreto == chute
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if(acertou):
-        print("Parabens! você acertou!")
+    if (acertou):
+        print("Você acertou!")
     else:
-        if(menor):
-            print("Você errou! O seu chute foi menor que o número secreto.")
-        elif(maior):
+        if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
-    rodada = rodada + 1
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-print("Fim de jogo")
+print("Fim do jogo")
