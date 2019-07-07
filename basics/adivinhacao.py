65bcc3870c68ebd38fb9a0a73eb29796680ddd66
Sir Felipe <felipe.dioliveira@outlook.com>
2019-07-06 21:20:25 -0300

    XPTO 4.1

--- basics/adivinhacao.py
+++ basics/adivinhacao.py
@@ -7,9 +7,13 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número de 1 a 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!  \n")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -17,10 +21,11 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
-            print("Você errou! O seu chute foi maior que o número secreto.")
+            print("Você errou! O seu chute foi maior que o número secreto.\n")
         elif (menor):
-            print("Você errou! O seu chute foi menor que o número secreto.")
+            print("Você errou! O seu chute foi menor que o número secreto.\n")
 
 print("Fim do jogo")
