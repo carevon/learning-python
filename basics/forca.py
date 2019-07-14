73bdf642bdc947e57c588642172749765cb1ec88
Sir Felipe <felipe.dioliveira@outlook.com>
2019-07-14 10:21:31 -0300

    XPTO

--- basics/forca.py
+++ basics/forca.py
@@ -2,6 +2,27 @@ def jogar():
     print("****************************************")
     print("Seja bem-vindo ao jogo de adivinhação")
     print("****************************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+
+        chute = input("Qual seu chute? ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+
+            if (chute.upper() == letra.upper()):
+                print("A palavra secreta contem a letra {} na posicao {}".format(chute, index))
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
