3e1b82611d2aa47b0c702407dcd5a9935cb4748e
Carlos Felipe Oliveira <felipe.dioliveira@outlook.com>
2019-08-01 14:07:34 -0300

    Python oo concepts

--- oo/conta.py
+++ oo/conta.py
@@ -2,7 +2,20 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo um objeto... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
