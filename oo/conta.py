666c552f775f57c284b9deb11b25f8ecf6ab619b
Carlos Felipe Oliveira <felipe.dioliveira@outlook.com>
2019-08-01 14:50:40 -0300

    Private methods

--- oo/conta.py
+++ oo/conta.py
@@ -7,6 +7,12 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
+    def get_titular(self):
+        return self.__titular
+
+    def get_numero(self):
+        return self.__numero
+
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
@@ -14,8 +20,32 @@ class Conta:
         self.__saldo += valor
 
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__disponibilidade_saques(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite.".format(valor))
+
+    def __disponibilidade_saques(self, valor_saque):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_saque <= valor_disponivel
 
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+
+
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    @saldo.setter
+    def saldo(self, saldo):
+        self.__saldo = saldo
