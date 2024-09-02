eec4a8e3037b115f8a2ee65edcd52a138e0dc2ff
carevon <felipe.dioliveira@outlook.com>
2024-09-02 00:33:41 -0300

    feat | implementing flask migrations

--- rest-api/flask_smorest/app.py
+++ rest-api/flask_smorest/app.py
@@ -4,6 +4,7 @@ import secrets
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -27,7 +28,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.environ.get("DATABASE_URL",  "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "jose"
